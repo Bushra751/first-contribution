186edccbdd7d9d41d2e594fbc1566d9264dddb8f
Zarish-123 <104060191+Zarish-123@users.noreply.github.com>
2022-05-23 12:53:12 +0500

    upates

--- linkedin/config.php
+++ linkedin/config.php
@@ -1,8 +1,8 @@
 <?php 
 session_start();
 
-define( 'CLIENT_ID' , '77yuq8efikgdeg');
-define( 'CLIENT_SECRET' , 'yrlcOFpu7p4LfGz4');
+define( 'CLIENT_ID' , '77stuf6s02sa1d');
+define( 'CLIENT_SECRET' , '3CeeVsZV9XhBXDvf');
 define( 'REDIRCET_URL' , 'http://localhost/linkedin/callback.php');
 define( 'SCOPES' , 'r_emailaddress,r_liteprofile,w_member_social');
 
