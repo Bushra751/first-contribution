9fe316c5b3775fd8c9b7ab25006b6435bf547ac8
bushra751 <bushrabib509@gmail.com>
2022-05-23 13:24:02 +0500

    h

--- linkedin/callback.php
+++ linkedin/callback.php
@@ -15,7 +15,7 @@ try {
         ],
     ]);
     $data = json_decode($response->getBody()->getContents(), true);
-    $access_token = $data['access_token']; // store this token somewhere
+    $access_token = $data['AQXYzHB_i3QvrVqkHIp5qnXXZcMMpBK5yzKuPJuCGJfqMYlDhi6gMBFOED04arvKLH9svsNEBnAiDDr54Cy174CpjNOSllxDXJ9kGOfJElUz0KDRUfO-Z7J9S8U_gYzs0H9E9H4QZlLpxANA-qsInsfIX9HQjo-w0AUFvglhDQnWpA9FuMtYBEo93ciWJToqlvt2V5aLusngO04JRrozP2aSq75MNikzItatOyE_CzYcRn5fgnjZgaYZhm11HpGhj4-EC0GZGimVw1HF9uToCUVKowB0-XhsECD3R5STz0M3pkw-QKFHONSGG40RhiwRByycQ1sxVo-rEadzktuW8zEQmyfyZA']; // store this token somewhere
     echo $access_token;
 } catch(Exception $e) {
     echo $e->getMessage();
